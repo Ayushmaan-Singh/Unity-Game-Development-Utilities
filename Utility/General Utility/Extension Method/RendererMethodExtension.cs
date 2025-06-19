0fc5033fd1207e02314b600802b41ad108de062f
Ayushmaan Singh <56771434+Ayushmaan-Singh@users.noreply.github.com>
2025-06-19 10:32:38 +0530

    Line Renderer Method Extension Reset

--- Utility/General Utility/Extension Method/RendererMethodExtension.cs
+++ Utility/General Utility/Extension Method/RendererMethodExtension.cs
@@ -12,7 +12,7 @@ namespace AstekUtility
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetFloat(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
@@ -22,29 +22,29 @@ namespace AstekUtility
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetFloatArray(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Int(this Renderer renderer, int propertyID, int val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetInteger(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Vector(this Renderer renderer, int propertyID, Vector4 val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetVector(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
@@ -54,18 +54,18 @@ namespace AstekUtility
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetVectorArray(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Matrix(this Renderer renderer, int propertyID, Matrix4x4 val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetMatrix(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
@@ -75,44 +75,50 @@ namespace AstekUtility
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetMatrixArray(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Texture(this Renderer renderer, int propertyID, Texture val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetTexture(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Buffer(this Renderer renderer, int propertyID, ComputeBuffer val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetBuffer(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
 		public static void ModifyMaterialProperty_Color(this Renderer renderer, int propertyID, Color val)
 		{
 			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
 			renderer.GetPropertyBlock(cachePropertyBlock);
 
 			cachePropertyBlock.SetColor(propertyID, val);
-			
+
 			renderer.SetPropertyBlock(cachePropertyBlock);
 			cachePropertyBlock.Clear();
 		}
-		
+
+		#endregion
+
+		#region Line Renderer
+
+		public static void Reset(this LineRenderer renderer) => renderer.positionCount = 0;
+
 		#endregion
 	}
 }
