6cf642b2d3e4856d2677c336156c0f3dd21e5cad
Ayushmaan Singh <56771434+Ayushmaan-Singh@users.noreply.github.com>
2025-06-19 08:37:51 +0530

    Renderer Method Extension
    -For modifying properties

---
+++ Utility/General Utility/Extension Method/RendererMethodExtension.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace AstekUtility
+{
+	public static class RendererMethodExtension
+	{
+		#region Modify Material Properties
+
+		public static void ModifyMaterialProperty_Float(this Renderer renderer, int propertyID, float val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetFloat(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		public static void ModifyMaterialProperty_FloatArray(this Renderer renderer, int propertyID, List<float> val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetFloatArray(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Int(this Renderer renderer, int propertyID, int val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetInteger(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Vector(this Renderer renderer, int propertyID, Vector4 val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetVector(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		public static void ModifyMaterialProperty_VectorArray(this Renderer renderer, int propertyID, List<Vector4> val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetVectorArray(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Matrix(this Renderer renderer, int propertyID, Matrix4x4 val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetMatrix(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		public static void ModifyMaterialProperty_MatrixArray(this Renderer renderer, int propertyID, List<Matrix4x4> val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetMatrixArray(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Texture(this Renderer renderer, int propertyID, Texture val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetTexture(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Buffer(this Renderer renderer, int propertyID, ComputeBuffer val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetBuffer(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		public static void ModifyMaterialProperty_Color(this Renderer renderer, int propertyID, Color val)
+		{
+			MaterialPropertyBlock cachePropertyBlock = new MaterialPropertyBlock();
+			renderer.GetPropertyBlock(cachePropertyBlock);
+
+			cachePropertyBlock.SetColor(propertyID, val);
+			
+			renderer.SetPropertyBlock(cachePropertyBlock);
+			cachePropertyBlock.Clear();
+		}
+		
+		#endregion
+	}
+}
