829d13d13f6f2aa9ff12957da6c314f2e0071870
Ayushmaan Singh <56771434+Ayushmaan-Singh@users.noreply.github.com>
2025-07-10 08:08:25 +0530

    To Generate 2D Arc Calculation

---
+++ Utility/Gameplay/Geometric Calculations/CalculatePositionsOnArc2D.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Gameplay.Strategem;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace AstekUtility.Gameplay.Geometric_Calculations
+{
+	[Serializable]
+	public class CalculatePositionsOnArc2D
+	{
+		private int _pointsOnArc = 5;
+
+		//Arc Properties
+		private Vector2 _centerPosition;
+		private Vector2 _arcCenterPosition;
+		private float _angleOffset;
+		private float _arcAngle;
+		private float _radius;
+
+		[ShowInInspector] public int PointsOnArc
+		{
+			get => _pointsOnArc;
+			set
+			{
+				_pointsOnArc = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+		[ShowInInspector, PropertyTooltip("Center Point Of Arc")] public Vector2 CenterPosition
+		{
+			get => _centerPosition;
+			set
+			{
+				_centerPosition = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+		[ShowInInspector, PropertyTooltip("Desired Center Point Of Arc")] public Vector2 ArcCenterPosition
+		{
+			get => _arcCenterPosition;
+			set
+			{
+				_arcCenterPosition = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+		[ShowInInspector] public float AngleOffset
+		{
+			get => _angleOffset;
+			set
+			{
+				_angleOffset = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+		[ShowInInspector, PropertyRange(0f, 360f)] public float ArcAngle
+		{
+			get => _arcAngle;
+			set
+			{
+				_arcAngle = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+		[ShowInInspector] public float Radius
+		{
+			get => _radius;
+			set
+			{
+				_radius = value;
+				PreCalculateCardPositionsAndTransformUp();
+			}
+		}
+
+
+		[ShowInInspector, EnableIf("@false")] public readonly List<Vector2> _position = new List<Vector2>();
+		[ShowInInspector, EnableIf("@false")] public readonly List<Vector2> _up = new List<Vector2>();
+
+		private void PreCalculateCardPositionsAndTransformUp()
+		{
+			float startAngle = -_arcAngle / 2;
+			float angleStep = _arcAngle / (_pointsOnArc - 1);
+
+			Vector2 centroid = Vector2.zero;
+
+			//Calculate Centroid
+			for (int i = 0; i < _pointsOnArc; i++)
+			{
+				float angle = startAngle + i * angleStep;
+				centroid += _centerPosition + (Vector2)(Quaternion.Euler(0, 0, angle) * Vector2.up * _radius);
+			}
+			centroid /= _pointsOnArc;
+
+			_position.Clear();
+			_up.Clear();
+			//Calculate position
+			for (int i = 0; i < _pointsOnArc; i++)
+			{
+				float angle = startAngle + i * angleStep;
+				Vector2 cardPos = _centerPosition + (Vector2)(Quaternion.Euler(0, 0, angle+_angleOffset) * Vector2.up * _radius);
+
+				Vector2 finalPos = cardPos + (_arcCenterPosition - centroid);
+				_position.Add(finalPos);
+				_up.Add((finalPos - _centerPosition).normalized);
+			}
+		}
+
+		public void OnDrawGizmos()
+		{
+			if (_position.Count < _pointsOnArc)
+				return;
+
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(_centerPosition, 0.1f);
+
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawSphere(_arcCenterPosition, 0.1f);
+
+			Gizmos.color = Color.green;
+			for (int i = 0; i < _pointsOnArc; i++)
+			{
+				Gizmos.DrawLine(_position[i], _position[i] - (_up[i] / 2f));
+			}
+		}
+	}
+}
---
+++ Utility/Gameplay/Geometric Calculations/TestArcGeometry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+namespace AstekUtility.Gameplay.Geometric_Calculations
+{
+	public class TestArcGeometry : MonoBehaviour
+	{
+		[SerializeField] private CalculatePositionsOnArc2D arcGeo;
+
+		[ShowInInspector] private readonly List<Transform> spawned = new List<Transform>();
+
+		[Button]
+		private void TestWithCardSpawn(GameObject obj)
+		{
+			for (int i = 0; i < spawned.Count; i++)
+			{
+				Transform go = spawned[0];
+				spawned.Remove(go);
+				DestroyImmediate(spawned[0].gameObject);
+			}
+
+			if (arcGeo._position.Count < arcGeo.PointsOnArc)
+				return;
+
+			for (int i = 0; i < arcGeo.PointsOnArc; i++)
+			{
+				GameObject instance = Instantiate(obj);
+				spawned.Add(instance.transform);
+				instance.transform.position = ((Vector3)arcGeo._position[i]).With(z:i * 0.1f);
+				instance.transform.up = arcGeo._up[i];
+			}
+		}
+
+		private void OnDrawGizmos()
+		{
+			arcGeo.OnDrawGizmos();
+
+			if (spawned.Count < arcGeo.PointsOnArc)
+				return;
+
+			for (int i = 0; i < spawned.Count; i++)
+			{
+				spawned[i].position = ((Vector3)arcGeo._position[i]).With(z:i * 0.1f);
+				spawned[i].up = arcGeo._up[i];
+			}
+		}
+	}
+}
