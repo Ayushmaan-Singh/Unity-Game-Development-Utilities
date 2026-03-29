64ef929a91b86aef21339bd4f56376b5b37fddad
Ayushmaan Singh <56771434+Ayushmaan-Singh@users.noreply.github.com>
2026-03-29 13:10:13 +0530

    Updated SpriteExtension
    Add SpriteOffset extension method to offset a sprite by exact pixel amount in comparison to sprite

--- Utility/General Utility/Extension Method/SpriteExtensions.cs
+++ Utility/General Utility/Extension Method/SpriteExtensions.cs
@@ -22,5 +22,30 @@ namespace Astek
         /// <exception cref="ArgumentNullException"></exception>
         public static Vector2 WorldScaledSize(this SpriteRenderer renderer) =>
             renderer == null ? throw new ArgumentNullException(nameof(renderer)) : renderer.sprite.WorldUnscaledSize() * renderer.transform.lossyScale;
+        
+        /// <summary>
+        /// Offset a sprite holding gameobject by certain pixel in comparison to sprite
+        /// </summary>
+        /// <param name="renderer"></param>
+        /// <param name="pixelOffset"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        public static void OffsetByPixels(this SpriteRenderer renderer, Vector2 pixelOffset)
+        {
+            if (renderer == null) 
+                throw new ArgumentNullException(nameof(renderer));
+            if(renderer.sprite == null)
+                throw new NullReferenceException(nameof(renderer.sprite));
+
+            // 1. Convert pixels to base world units
+            float ppu = renderer.sprite.pixelsPerUnit;
+            Vector2 unitOffset = pixelOffset / ppu;
+
+            // 2. Account for the GameObject's scale
+            Vector3 worldOffset = Vector3.Scale(unitOffset, renderer.transform.lossyScale);
+
+            // 3. Apply to position
+            renderer.transform.position += worldOffset;
+        }
     }
 }
